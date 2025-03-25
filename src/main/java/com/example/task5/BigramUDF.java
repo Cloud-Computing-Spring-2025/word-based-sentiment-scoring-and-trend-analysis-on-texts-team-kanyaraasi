1c88e746152253540c55ffa9e0c8ffdbbcd0cc4b
saiharsha paladugu <saiharshapaladugu@Mac-15128.lan>
2025-03-24 22:29:27 -0400

    Task 5 Bigram

--- src/main/java/com/example/task5/BigramUDF.java
+++ src/main/java/com/example/task5/BigramUDF.java
@@ -1,5 +1,31 @@
 package com.example.task5;
 
-public class BigramUDF {
-    
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BigramUDF extends UDF {
+    public List<String> evaluate(Text input) {
+        if (input == null) {
+            return null;
+        }
+
+        String text = input.toString().toLowerCase().replaceAll("[^a-z\\s]", "");
+        StringTokenizer tokenizer = new StringTokenizer(text);
+        List<String> words = new ArrayList<>();
+
+        while (tokenizer.hasMoreTokens()) {
+            words.add(tokenizer.nextToken());
+        }
+
+        List<String> bigrams = new ArrayList<>();
+        for (int i = 0; i < words.size() - 1; i++) {
+            bigrams.add(words.get(i) + " " + words.get(i + 1));
+        }
+
+        return bigrams;
+    }
 }
+
